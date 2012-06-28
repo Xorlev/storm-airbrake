eec751d28b5e1dbd578818c41b558bf5651a57f4
Michael Rose <elementation@gmail.com>
2012-06-28 10:05:15 -0600

    Making storm a dev dep

--- project.clj
+++ project.clj
@@ -6,7 +6,8 @@
   :min-lein-version "2.0"
   :dependencies [[org.codehaus.groovy/groovy-all "1.8.6"]
                  [org.apache.httpcomponents/httpclient "4.2-alpha1"]
-                 [storm "0.7.2"]]
+                 ]
+  :dev-dependencies [[storm "0.7.2"]]
 ;; deployment password should be specified in LIEN_PASSWORD
   :deploy-repositories  {"snapshots" {:url "https://fullcontact.artifactoryonline.com/fullcontact/libs-snapshots-local"}
                          "releases" {:url "https://fullcontact.artifactoryonline.com/fullcontact/libs-releases-local"}}
